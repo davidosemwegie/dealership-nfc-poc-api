d30373ce4758c92ae92800f2abbfdd7b3c5d39b4
David Osemwegie <dosemwegie@gmail.com>
2021-09-05 20:14:37 -0600

    make it better

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export interface EnvVariables {
   ACCESS_TOKEN_SECRET: string;
   PORT: string;
   MAX_FACES: number;
-  DB_CONNECTION_STRING: string;
+  DATABASE_URL: string;
 }
 
-export const { ACCESS_TOKEN_SECRET, PORT, MAX_FACES, DB_CONNECTION_STRING } = process.env as unknown as EnvVariables;
+export const { ACCESS_TOKEN_SECRET, PORT, MAX_FACES, DATABASE_URL } = process.env as unknown as EnvVariables;
